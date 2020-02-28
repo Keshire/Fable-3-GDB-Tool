924c056d83fd641e3b70aff2c58b2006a2b6361d
Keshire <keshire_the_chaotic@hotmail.com>
2020-02-28 13:41:23 -0600

    Repository Cleanup and Add/Ignores

---
+++ GDBEditor/Source/DataTypes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDBEditor
+{
+    class DataTypes
+    {
+        //0000 = boolean
+        //0100 = dword
+        //0200 = dword lots of GroupIndex
+        //0300 = float
+        //0400 = string hash
+        //0500 = numeric indicating an enumerated type
+        //0600 = object hash
+        //0700 = object hash
+
+        public bool data_type_0000 { get; set; }
+        public UInt32 data_type_0100 { get; set; }
+        public UInt32 data_type_0200 { get; set; }
+        public float data_type_0300 { get; set; }
+        public UInt32 data_type_0400 { get; set; }
+        public UInt32 data_type_0500 { get; set; }
+        public UInt32 data_type_0600 { get; set; }
+        public UInt32 data_type_0700 { get; set; }
+
+    }
+}
---
+++ GDBEditor/Source/GDB_Util.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace GDBEditor
+{
+    public class GDB_Util
+    {
+        //Just in case I need to convert a string back to hash
+        public static uint FnvHash(string str)
+        {
+            long num = -2128831035;
+            for (int i = 0; i <= str.Length - 1; i++)
+            {
+                num = (num * 16777619 & -1);
+                num ^= (65535 & Convert.ToUInt32(str[i]));
+            }
+            return (uint)num;
+        }
+    }
+}
